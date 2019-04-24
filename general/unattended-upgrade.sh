5d01e1d39229c2c49d2d3ea3e3c4bd48445ce305
katamo <kat.morgan@braincraft.io>
2019-04-24 06:25:00 -1000

    add release upgrade script

---
+++ general/unattended-upgrade.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Full Release Upgrade (Unattended)
+################################################################################
+# Export Vars
+export UCF_FORCE_CONFFOLD=YES
+export DEBIAN_FRONTEND=noninteractive
+
+################################################################################
+# Refresh Sources
+sudo apt-get update
+
+################################################################################
+# Upgrade / Dist Upgrade / Clean
+sudo apt-get --option=Dpkg::Options::=--force-confold \
+             --option=Dpkg::options::=--force-unsafe-io \
+             --assume-yes --quiet \
+             upgrade
+
+sudo apt-get --option=Dpkg::Options::=--force-confold \
+             --option=Dpkg::options::=--force-unsafe-io \
+             --assume-yes --quiet \
+             dist-upgrade
+
+sudo apt-get --option=Dpkg::Options::=--force-confold \
+             --option=Dpkg::options::=--force-unsafe-io \
+             --assume-yes --quiet \
+             autoremove
+
+################################################################################
+# Upgrade to new release
+sudo do-release-upgrade -q -d -f DistUpgradeViewNonInteractive
